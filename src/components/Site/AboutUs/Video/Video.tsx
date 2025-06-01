ac64a3b4c6f1ce2b21eaa4918853ace8e7021224
Viktoriia Kolesnik <vika.notariat@ukr.net>
2025-06-01 12:59:28 +0300

    refactor(about-us): добавила видео из ютуб

--- src/components/Site/AboutUs/Video/Video.tsx
+++ src/components/Site/AboutUs/Video/Video.tsx
@@ -9,10 +9,14 @@ const Video = ({}: Props) => {
     return (
         <Container>
             <div className={style.img}>
-                <img
-                    src='/images/video.jpg'
-                    alt=''
-            />
+                <iframe
+                        src="https://www.youtube.com/embed/2jxeMWmoTSY?si=kIVhVrIAItIHs8sw"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                ></iframe>
             </div>
         </Container>
     );
