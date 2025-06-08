24ec45a9486da18936d0b44e6fb48ce2380347be
Viktoriia Kolesnik <vika.notariat@ukr.net>
2025-06-08 13:26:23 +0300

    feature(sidebar): добавила файл dropdown

---
+++ src/components/Admin/Sidebar/NavMain.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { ChevronRight, type LucideIcon } from "lucide-react"
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link";
+import {IconType} from "react-icons";
+
+interface DropdownItem {
+    title: string,
+    url:string
+}
+
+interface DropdownList {
+    title:string,
+    url:string,
+    icon:IconType ,
+    isActive: boolean,
+    items:DropdownItem[]
+}
+interface Props {
+    item:DropdownList
+}
+
+
+
+export function NavMain({item}:Props) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarMenu>
+                    <Collapsible
+                        asChild
+                        className="group/collapsible"
+                    >
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger asChild>
+                                <SidebarMenuButton tooltip={item.title}>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                                </SidebarMenuButton>
+                            </CollapsibleTrigger>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    {item.items?.map((subItem) => (
+                                        <SidebarMenuSubItem key={subItem.title}>
+                                            <SidebarMenuSubButton asChild>
+                                                <Link href={subItem.url}>
+                                                    <span>{subItem.title}</span>
+                                                </Link>
+                                            </SidebarMenuSubButton>
+                                        </SidebarMenuSubItem>
+                                    ))}
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+            </SidebarMenu>
+        </SidebarGroup>
+    )
+}
+
+
+
+
+
+
