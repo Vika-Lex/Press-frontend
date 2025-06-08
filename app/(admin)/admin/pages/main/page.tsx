8730462d5a9403f502bcad78aaeddb656266b65f
Viktoriia Kolesnik <vika.notariat@ukr.net>
2025-06-08 13:25:23 +0300

    feature(sidebar): добавила main страничку для редактирования

---
+++ app/(admin)/admin/pages/main/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/src/components/Admin/Pages/Main/Hero/Hero";
+
+interface Props {
+    className?: string
+}
+
+
+const Page = ({}: Props) => {
+    return (
+        <>
+            <Hero/>
+        </>
+    );
+};
+export default Page
