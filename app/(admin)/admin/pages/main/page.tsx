050ca94c41e425a14fae00a8c839bcc06adbf581
Viktoriia Kolesnik <vika.notariat@ukr.net>
2025-06-14 15:53:05 +0300

    feat(sidebar): добавлены компоненты MainList, Benefits и Clients в Page, а также все компоненты обернуты в flex-контейнер

--- app/(admin)/admin/pages/main/page.tsx
+++ app/(admin)/admin/pages/main/page.tsx
@@ -1,4 +1,7 @@
 import Hero from "@/src/components/Admin/Pages/Main/Hero/Hero";
+import MainList from "@/src/components/Admin/Pages/Main/MainList/MainList";
+import Benefits from "@/src/components/Admin/Pages/Main/Benefits/Benefits";
+import Clients from "@/src/components/Admin/Pages/Main/Clients/Clients";
 
 interface Props {
     className?: string
@@ -7,9 +10,12 @@ interface Props {
 
 const Page = ({}: Props) => {
     return (
-        <>
+        <div className='flex flex-col'>
             <Hero/>
-        </>
+            <MainList/>
+            <Benefits/>
+            <Clients/>
+        </div>
     );
 };
 export default Page
